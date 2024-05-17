ce3aede462d857541d8420e750d7069d3054f5dc
Francis De Brabandere <francisdb@gmail.com>
2024-05-17 10:31:51 +0200

    use https urls for fetching test scripts

--- testscripts/populate.sh
+++ testscripts/populate.sh
@@ -5,10 +5,10 @@ set -e
 cd "$(dirname "$0")"
 
 rm -rf vpx-standalone-scripts
-git clone --depth 1 git@github.com:jsm174/vpx-standalone-scripts.git
+git clone --depth 1 https://github.com/jsm174/vpx-standalone-scripts.git
 
 rm -rf vpinball
-git clone --depth 1 git@github.com:vpinball/vpinball.git
+git clone --depth 1 https://github.com/vpinball/vpinball.git
 
 rm -rf vpxtable-scripts
-git clone git@github.com:sverrewl/vpxtable_scripts.git
+git clone --depth 1 https://github.com/sverrewl/vpxtable_scripts.git
