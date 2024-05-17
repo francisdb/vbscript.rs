296191fbe9003d8b690c15efb23759d45c03b6f8
Francis De Brabandere <francisdb@gmail.com>
2024-05-17 10:39:15 +0200

    smarter example script updates

--- testscripts/populate.sh
+++ testscripts/populate.sh
@@ -4,11 +4,24 @@ set -e
 # make sure we are in the correct directory
 cd "$(dirname "$0")"
 
-rm -rf vpx-standalone-scripts
-git clone --depth 1 https://github.com/jsm174/vpx-standalone-scripts.git
+repos=(
+  "vpx-standalone-scripts https://github.com/jsm174/vpx-standalone-scripts.git"
+  "vpinball https://github.com/vpinball/vpinball.git"
+  "sverrewl-vpxtable-scripts https://github.com/sverrewl/vpxtable_scripts.git"
+)
 
-rm -rf vpinball
-git clone --depth 1 https://github.com/vpinball/vpinball.git
+for repo in "${repos[@]}"; do
+  IFS=' ' read -r -a array <<< "$repo"
+  dir=${array[0]}
+  url=${array[1]}
 
-rm -rf vpxtable-scripts
-git clone --depth 1 https://github.com/sverrewl/vpxtable_scripts.git
+  if [ -d "$dir" ]; then
+    echo "Updating $dir"
+    cd "$dir"
+    git pull
+    cd ..
+  else
+    echo "Cloning $dir"
+    git clone --depth 1 "$url" "$dir"
+  fi
+done
